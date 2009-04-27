f30013e274dc78665fe1de7857db9b8b005e3467
Jonathan Yu <jawnsy@cpan.org>
2009-04-27 01:05:33 +0000

    - Updated to version 1.1.4, prepare for next release

--- lib/Test/DistManifest.pm
+++ lib/Test/DistManifest.pm
@@ -21,11 +21,11 @@ exists, excluding those in your MANIFEST.SKIP
 
 =head1 VERSION
 
-Version 1.1.3 ($Id$)
+Version 1.1.4 ($Id$)
 
 =cut
 
-use version; our $VERSION = qv('1.1.3');
+use version; our $VERSION = qv('1.1.4');
 
 =head1 EXPORTS
 
