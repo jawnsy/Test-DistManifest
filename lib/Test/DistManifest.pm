b9659184f5d69577eca04842b7d0520e6a75debf
Jonathan Yu <jawnsy@cpan.org>
2009-12-29 01:49:23 +0000

    bump to 1.009

--- lib/Test/DistManifest.pm
+++ lib/Test/DistManifest.pm
@@ -15,11 +15,11 @@ Test::DistManifest - Author test that validates a package MANIFEST
 
 =head1 VERSION
 
-Version 1.008 ($Id$)
+Version 1.009 ($Id$)
 
 =cut
 
-our $VERSION = '1.008';
+our $VERSION = '1.009';
 $VERSION = eval $VERSION;
 
 =head1 EXPORTS
